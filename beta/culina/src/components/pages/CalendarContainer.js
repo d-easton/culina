16412d42dbc7deb9af67769d2250b4a36427186c
David Easton <d.easton@wustl.edu>
2020-12-07 11:04:52 -0600

    card stuff

--- beta/culina/src/components/pages/CalendarContainer.js
+++ beta/culina/src/components/pages/CalendarContainer.js
@@ -3,7 +3,7 @@ import '../../App.css';
 import './Calendar/css/Calendar.css';
 const axios = require('axios');
 const getCalendarURL = "https://cors-anywhere.herokuapp.com/http://35.193.28.175:8085/getGroceryList";
-
+// this is an incorrect url, just there to test ^
 
 class CalendarContainer extends React.Component {
     __mounted = false;
@@ -63,13 +63,41 @@ class CalendarContainer extends React.Component {
             <div id="calendar-wrapper" className="grey">
                 <div id="calendar-div" className="blue-dark"></div>
                 <div id="calendar-wrapper">
-                    <div id="calendar-day-sun" className="calendar-day"></div>
-                    <div id="calendar-day-mon" className="calendar-day"></div>
-                    <div id="calendar-day-tue" className="calendar-day"></div>
-                    <div id="calendar-day-wed" className="calendar-day"></div>
-                    <div id="calendar-day-thu" className="calendar-day"></div>
-                    <div id="calendar-day-fri" className="calendar-day"></div>
-                    <div id="calendar-day-sat" className="calendar-day"></div>
+                    <div id="calendar-day-sun" className="calendar-day">
+                        <h4 className="card-title">Sunday</h4>
+                        <div className="bar"></div>
+                        <div className="card-recipes"></div>
+                    </div>
+                    <div id="calendar-day-mon" className="calendar-day">
+                        <h4 className="card-title">Monday</h4>
+                        <div className="bar"></div>
+                        <div className="card-recipes"></div>
+                    </div>
+                    <div id="calendar-day-tue" className="calendar-day">
+                        <h4 className="card-title">Tuesday</h4>
+                        <div className="bar"></div>
+                        <div className="card-recipes"></div>
+                    </div>
+                    <div id="calendar-day-wed" className="calendar-day">
+                        <h4 className="card-title">Wednesday</h4>
+                        <div className="bar"></div>
+                        <div className="card-recipes"></div>
+                    </div>
+                    <div id="calendar-day-thu" className="calendar-day">
+                        <h4 className="card-title">Thursday</h4>
+                        <div className="bar"></div>
+                        <div className="card-recipes"></div>
+                    </div>
+                    <div id="calendar-day-fri" className="calendar-day">
+                        <h4 className="card-title">Friday</h4>
+                        <div className="bar"></div>
+                        <div className="card-recipes"></div>
+                    </div>
+                    <div id="calendar-day-sat" className="calendar-day">
+                        <h4 className="card-title">Saturday</h4>
+                        <div className="bar"></div>
+                        <div className="card-recipes"></div>
+                    </div>
                 </div>
                 {/* <div id="recipes-div" className="sand-dark"></div> */}
             </div>
