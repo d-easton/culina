c924ebe09f9720c40a11e06598cc42aaf59ef39d
David Easton <d.easton@wustl.edu>
2020-12-09 20:20:37 -0600

    v dumb bug fixed

--- beta/culina/src/components/pages/Calendar/model/Card.jsx
+++ beta/culina/src/components/pages/Calendar/model/Card.jsx
@@ -16,7 +16,7 @@ const CardDiv = styled.div`
 export default class Card extends React.Component {
     render() {
         //isDragDisabled={this.props.isDisabled}
-        console.log(this.props.recipe.header.id);
+        // console.log(this.props.recipe.header.id);
         return (
             <Draggable draggableId={this.props.recipe.header.id} index={this.props.index} >  
                 { provided => (
