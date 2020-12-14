7dcaac8fd24c39e632623e27fb916e0042674788
Jeremie Osaghae-Nosa <jeremienosa@gmail.com>
2020-12-14 14:20:53 -0600

    reorder navbar

--- beta/culina/src/components/pages/Navbar.js
+++ beta/culina/src/components/pages/Navbar.js
@@ -1,63 +1,55 @@
-import React from 'react';
-import * as ROUTE from 'react-router-dom'
-import * as RBS from 'react-bootstrap';
-import './css/Navbar.css';
-
+import React from "react";
+import * as ROUTE from "react-router-dom";
+import * as RBS from "react-bootstrap";
+import "./css/Navbar.css";
 
 const Navbar = (props) => {
+  const { user, handleSignout } = props;
 
-    const { user, handleSignout } = props;
-
-    return (
-
-        <nav className="fix">
-            {user ? (
-                <RBS.Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-                    <RBS.Navbar.Brand href="/">
-                        Culina
-                        <i className="fas fa-stroopwafel"></i>
-                    </RBS.Navbar.Brand>
-                    <RBS.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <RBS.Navbar.Collapse id="responsive-navbar-nav">
-                        <RBS.Nav className="mr-auto">
-                        </RBS.Nav>
-                        <RBS.Nav>
-                            <RBS.Nav.Link href="/grocery-list">Grocery List</RBS.Nav.Link>
-                            <RBS.Nav.Link href="/recipe-list">Recipes</RBS.Nav.Link>
-                            <RBS.Nav.Link href="/calendar">Calendar</RBS.Nav.Link>
-                            <RBS.Nav.Link href="/top">Top</RBS.Nav.Link>
-                            {/* <RBS.Nav.Link href="/services">Services</RBS.Nav.Link>
+  return (
+    <nav className="fix">
+      {user ? (
+        <RBS.Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+          <RBS.Navbar.Brand href="/">
+            Culina
+            <i className="fas fa-stroopwafel"></i>
+          </RBS.Navbar.Brand>
+          <RBS.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <RBS.Navbar.Collapse id="responsive-navbar-nav">
+            <RBS.Nav className="mr-auto"></RBS.Nav>
+            <RBS.Nav>
+              <RBS.Nav.Link href="/top">Top Recipes</RBS.Nav.Link>
+              <RBS.Nav.Link href="/grocery-list">Grocery List</RBS.Nav.Link>
+              <RBS.Nav.Link href="/recipe-list">My Recipes</RBS.Nav.Link>
+              <RBS.Nav.Link href="/calendar">Calendar</RBS.Nav.Link>
+              {/* <RBS.Nav.Link href="/services">Services</RBS.Nav.Link>
                             <RBS.Nav.Link href="/products">Products</RBS.Nav.Link> */}
-                        </RBS.Nav>
-                        <RBS.Nav>
-                            <RBS.Nav.Link onClick={handleSignout} href="/">Sign-Out</RBS.Nav.Link>
-                        </RBS.Nav>
-                    </RBS.Navbar.Collapse>
-                </RBS.Navbar>
-            ) 
-            : 
-            (
-                <RBS.Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-                    <RBS.Navbar.Brand href="/">
-                        Culina
-                        <i class="fas fa-stroopwafel"></i>
-                    </RBS.Navbar.Brand>
-                    <RBS.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <RBS.Navbar.Collapse id="responsive-navbar-nav">
-                        <RBS.Nav className="mr-auto">
-                        </RBS.Nav>
-                        <RBS.Nav>
-                            <RBS.Nav.Link href="/sign-in">Sign-In</RBS.Nav.Link>
-                            <RBS.Nav.Link href="/sign-up">Sign-Up</RBS.Nav.Link>
-                        </RBS.Nav>
-                    </RBS.Navbar.Collapse>
-                </RBS.Navbar>
-            )
-            }
-
-        </nav>    
-    );
-
+            </RBS.Nav>
+            <RBS.Nav>
+              <RBS.Nav.Link onClick={handleSignout} href="/">
+                Sign-Out
+              </RBS.Nav.Link>
+            </RBS.Nav>
+          </RBS.Navbar.Collapse>
+        </RBS.Navbar>
+      ) : (
+        <RBS.Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+          <RBS.Navbar.Brand href="/">
+            Culina
+            <i class="fas fa-stroopwafel"></i>
+          </RBS.Navbar.Brand>
+          <RBS.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <RBS.Navbar.Collapse id="responsive-navbar-nav">
+            <RBS.Nav className="mr-auto"></RBS.Nav>
+            <RBS.Nav>
+              <RBS.Nav.Link href="/sign-in">Sign-In</RBS.Nav.Link>
+              <RBS.Nav.Link href="/sign-up">Sign-Up</RBS.Nav.Link>
+            </RBS.Nav>
+          </RBS.Navbar.Collapse>
+        </RBS.Navbar>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
