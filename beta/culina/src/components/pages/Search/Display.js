94c6d68b29a71922bb684656e9ab98f6d72773d5
Jeremie Osaghae-Nosa <jeremienosa@gmail.com>
2020-12-15 11:28:21 -0600

    fix

--- beta/culina/src/components/pages/Search/Display.js
+++ beta/culina/src/components/pages/Search/Display.js
@@ -23,7 +23,7 @@ const Display = (props) => {
         .catch((err) => console.log("err", err));
     }
     fetchRecipes();
-  }, [email]);
+  }, []);
 
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
