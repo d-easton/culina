ce5529f063aee42b8d59bc77fd36325b8384ffec
David Easton <d.easton@wustl.edu>
2021-01-09 17:25:37 -0500

    first bit of ingredients

---
+++ release/culina/src/components/pages/GroceryList/constants.js
@@ -0,0 +1,741 @@
+
+const ingredientDatabase = {
+    // MARK: BAKING
+    "BAKING_POWDER": {
+        "display_name": "baking powder",
+        "translations": [
+
+        ]
+    },
+    "BAKING_SODA": {
+        "display_name": "baking soda",
+        "translations": [
+
+        ]
+    },
+    "BROWN_SUGAR": {
+        "display_name": "brown sugar",
+        "translations": [
+
+        ]
+    },
+    "COCOA_POWDER": {
+        "display_name": "cocoa powder",
+        "translations": [
+
+        ]
+    },
+    "CORN_STARCH": {
+        "display_name": "corn starch",
+        "translations": [
+
+        ]
+    },
+    "CORN_STARCH": {
+        "display_name": "corn starch",
+        "translations": [
+
+        ]
+    },
+    "FLOUR": {
+        "display_name": "flour",
+        "translations": [
+            "white flour",
+            "baking flour",
+        ]
+    },
+    "HONEY": {
+        "display_name": "honey",
+        "translations": [
+
+        ]
+    },
+    "IODIZED_SALT":{
+        "display_name": "iodized salt",
+        "translations": [
+            
+        ]
+    },
+    "KOSHER_SALT":{
+        "display_name": "Kosher salt",
+        "translations": [
+            
+        ]
+    },
+    "MOLASSES": {
+        "display_name": "molasses",
+        "translations": [
+
+        ]
+    },
+    "SALT":{
+        "display_name": "salt",
+        "translations": [
+
+        ]
+    },
+    "SEA_SALT":{
+        "display_name": "sea salt",
+        "translations": [
+            
+        ]
+    },
+    "STEVIA" : {
+        "display_name": "stevia sweetener",
+        "translations": [
+
+        ]
+    },
+    "TURBINADO_SUGAR": {
+        "display_name": "turbinado sugar",
+        "translations": [
+
+        ]
+    },
+    "WHITE_SUGAR" : {
+        "display_name": "granulated white sugar",
+        "translations": [
+
+        ]
+    },
+    "YEAST": {
+        "display_name": "yeast",
+        "translations": [
+
+        ]
+    },
+    // MARK: OIL & VINEGAR
+    "APPLE_CIDER_VINEGAR": {
+        "display_name": "apple cider vinegar",
+        "translations": [
+
+        ]
+    },
+    "BALSAMIC_VINEGAR": {
+        "display_name": "balsamic vinegar",
+        "translations": [
+            "balsamico",
+            "modena vinegar",
+            "aceto balsamico di modena"
+        ]
+    },
+    "COCONUT_OIL" : {
+        "display_name": "coconut oil",
+        "translations": [
+    
+        ]
+    },
+    "OLIVE_OIL":{
+        "display_name": "extra virgin olive oil",
+        "translations": [
+            "olive oil",
+            "evoo"
+        ]
+    },
+    "PEANUT_OIL" : {
+        "display_name": "peanut oil",
+        "translations": [
+    
+        ]
+    },
+    "RED_WINE_VINEGAR" : {
+        "display_name": "red wine vinegar",
+        "translations": [
+           
+        ]
+    },
+    "SESAME_SEED_OIL" : {
+        "display_name": "sesame seed oil",
+        "translations": [
+    
+        ]
+    },
+    "TOASTED_SESAME_SEED_OIL" : {
+        "display_name": "toasted sesame seed oil",
+        "translations": [
+    
+        ]
+    },
+    "VEGETABLE_OIL" : {
+        "display_name": "vegetable oil",
+        "translations": [
+            "canola_oil"
+        ]
+    },
+    "VINEGAR": {
+        "display_name": "vinegar",
+        "translations": [
+            "white vinegar",
+            "regular vinegar",
+            "regular white vinegar"
+        ]
+    },  
+    "WHITE_WINE_VINEGAR" : {
+        "display_name": "white wine vinegar",
+        "translations": [
+            "white vinegar",
+            "regular vinegar",
+            "regular white vinegar"
+        ]
+    },
+    // SPICES & HERBS
+    "ALLSPICE": {
+        "display_name": "allspice",
+        "translations": [
+            "chinese_allspice"
+        ]
+    },
+    "ANISE": {
+        "display_name": "anise",
+        "translations": [
+            "star anise"
+        ]
+    },
+    "BASIL": {
+        "display_name": "basil",
+        "translations": [
+
+        ]
+    },
+    "CAJUN_SEASONING": {
+        "display_name": "cajun seasoning",
+        "translations": [
+            "creole seasoning"
+        ]
+    },
+    "CHILI_POWDER": {
+        "display_name": "chili powder",
+        "translations": [
+
+        ]
+    },
+    "CHIVES":{
+        "display_name": "chives",
+        "translations": [
+
+        ]
+    },
+    "CINNAMON": {
+        "display_name": "cinnamon",
+        "translations": [
+
+        ]
+    },
+    "CORIANDER": {
+        "display_name": "coriander",
+        "translations": [
+            "cilantro"
+        ]
+    },
+    "CUMIN": {
+        "display_name": "cumin",
+        "translations": [
+            
+        ]
+    },
+    "DILL": {
+        "display_name": "dill",
+        "translations": [
+
+        ]
+    },
+    "FENNEL": {
+        "display_name": "fennel",
+        "translations": [
+            
+        ]
+    },
+    "GARLIC": {
+        "display_name": "garlic",
+        "translations": [
+
+        ]
+    },
+    "GARLIC_POWDER": {
+        "display_name": "garlic powder",
+        "translations": [
+            
+        ]
+    },
+    "GREEN_ONIONS": {
+        "display_name": "green onions",
+        "translations": [
+            
+        ]
+    },
+    "HERB_DE_PROVENCE": {
+        "display_name": "Herb de Provence",
+        "translations": [
+            "herbs de provence",
+            "herbs du provence",
+            "herb du provence",
+        ]
+    },
+    "FRESH_BASIL": {
+        "display_name": "fresh basil",
+        "translations": [
+            
+        ]
+    },
+    "FRESH_CORIANDER": {
+        "display_name": "fresh coriander",
+        "translations": [
+            "fresh cilantro"
+        ]
+    },
+    "FRESH_DILL": {
+        "display_name": "fresh dill",
+        "translations": [
+            
+        ]
+    },
+    "FRESH_OREGANO": {
+        "display_name": "fresh oregano",
+        "translations": [
+            
+        ]
+    },
+    "FRESH_PARSLEY": {
+        "display_name": "fresh parlsey",
+        "translations": [
+            
+        ]
+    },
+    "FRESH_ROSEMARY": {
+        "display_name": "fresh rosemary",
+        "translations": [
+            
+        ]
+    },
+    "FRESH_THYME": {
+        "display_name": "fresh thyme",
+        "translations": [
+            
+        ]
+    },
+    "PARSLEY": {
+        "display_name": "parlsey",
+        "translations": [
+            
+        ]
+    },
+    "NUTMEG": {
+        "display_name": "nutmeg",
+        "translations": [
+
+        ]
+    },
+    "OREGANO": {
+        "display_name": "oregano",
+        "translations": [
+
+        ]
+    },
+    "SICHUAN_PEPPERCORN": {
+        "display_name": "sichuan peppercorn",
+        "translations": [
+            "szechuan pepper",
+            "szechwan pepper",
+            "Chinese prickly ash",
+            "Chinese pepper",
+            "rattan pepper",
+            "mala pepper"
+        ]
+    },
+    "TERRAGON": {
+        "display_name": "terragon",
+        "translations": [
+            
+        ]
+    },
+    "THYME": {
+        "display_name": "thyme",
+        "translations": [
+            
+        ]
+    },
+    "ROSEMARY": {
+        "display_name": "rosemary",
+        "translations": [
+            
+        ]
+    },
+    // VEGETABLES
+    "ARUGULA" : {
+        "display_name": "arugula",
+        "translations": [
+            "rocket"
+        ]
+    },
+    "ASPARAGUS" : {
+        "display_name": "asparagus",
+        "translations": [
+
+        ]
+    },
+    "BROCCOLI" : {
+        "display_name": "broccoli",
+        "translations": [
+            
+        ]
+    },
+    "BROCCOLINI" : {
+        "display_name": "broccolini",
+        "translations": [
+            
+        ]
+    },
+    "BRUSSELS_SPROUTS" : {
+        "display_name": "Brussels sprouts",
+        "translations": [
+            
+        ]
+    },
+    "CARROT" : {
+        "display_name": "carrot",
+        "translations": [
+            
+        ]
+    },
+    "CAULIFLOWER" : {
+        "display_name": "cauliflower",
+        "translations": [
+            
+        ]
+    },
+    "CUCUMBER" : {
+        "display_name": "cucumber",
+        "translations": [
+            
+        ]
+    },
+    "ENGLISH_CUCUMBER" : {
+        "display_name": "English cucumber",
+        "translations": [
+            "seedless cucumber",
+            "hothouse cucumber"
+        ]
+    },
+    "GINGER ROOT" : {
+        "display_name": "ginger root",
+        "translations": [
+            "ginger",
+            "fresh ginger"
+        ]
+    },
+    "GLOBE_EGGPLANT" : {
+        "display_name": "globe eggplant",
+        "translations": [
+            "eggplant"
+        ]
+    },
+    "GREEN_BEANS" : {
+        "display_name": "green beans",
+        "translations": [
+            
+        ]
+    },
+    "JAPANESE_EGGPLANT" : {
+        "display_name": "Japanese eggplant",
+        "translations": [
+            
+        ]
+    },
+    "MUSHROOMS": {
+        "display_name": "mushrooms",
+        "translations": [
+
+        ]
+    },
+    "RED_ONION": {
+        "display_name": "red onion",
+        "translations": [
+            
+        ]
+    },
+    "SHALLOTS": {
+        "display_name": "shallots",
+        "translations": [
+            
+        ]
+    },
+    "SPINACH" : {
+        "display_name": "spinach",
+        "translations": [
+            
+        ]
+    },
+    "SQUASH": {
+        "display_name": "squash",
+        "translations": [
+            
+        ]
+    },
+    "SHALLOTS": {
+        "display_name": "shallots",
+        "translations": [
+            
+        ]
+    },
+    "TOMATO" : {
+        "display_name": "tomato",
+        "translations": [
+            
+        ]
+    },
+    "VIDALIA_ONION": {
+        "display_name": "vidalia onion",
+        "translations": [
+            
+        ]
+    },
+    "WHITE_ONION": {
+        "display_name": "white onion",
+        "translations": [
+            
+        ]
+    },
+    "YELLOW_ONION": {
+        "display_name": "yellow onion",
+        "translations": [
+            
+        ]
+    },
+    "ZUCCHINI" : {
+        "display_name": "zucchini",
+        "translations": [
+            
+        ]
+    },
+    // FRUITS
+    "AVOCADO" : {
+        "display_name": "avocado",
+        "translations": [
+
+        ]
+    },
+    "RED_DELICIOUS_APPLES" : {
+        "display_name": "red delicious apples",
+        "translation": [
+            
+        ]
+    },
+    "GALA_APPLES" : {
+        "display_name": "gala apples",
+        "translation": [
+            
+        ]
+    },
+    "GREEN_APPLES" : {
+        "display_name": "green apples",
+        "translation": [
+            "granny smith apples"
+        ]
+    },
+    "BANANA" : {
+        "display_name": "banana",
+        "translations": [
+
+        ]
+    },
+    "BLUEBERRY" : {
+        "display_name": "blueberry",
+        "translation": [
+            
+        ]
+    },
+    "CLEMENTINES" : {
+        "display_name": "clementines",
+        "translations": [
+            "tangor"
+        ]
+    },
+    "GRAPEFRUIT" : {
+        "display_name": "grapefruit",
+        "translations": [
+
+        ]
+    },
+    "LEMON" : {
+        "display_name": "lemon",
+        "translation": [
+
+        ]
+    },
+    "LIME" : {
+        "display_name": "lime",
+        "translation": [
+            
+        ]
+    },
+    "MANGO" : {
+        "display_name": "mango",
+        "translation": [
+            
+        ]
+    },
+    "MIXED_BERRIES" : {
+        "display_name": "mixed berries",
+        "translations": [
+            "mixed berries"
+        ]
+    },
+    "ORANGE" : {
+        "display_name": "orange",
+        "translation": [
+            
+        ]
+    },
+    "PINEAPPLE" : {
+        "display_name": "pineapple",
+        "translation": [
+            
+        ]
+    },
+    "RASPBERRY" : {
+        "display_name": "RASPBERRY",
+        "translation": [
+            
+        ]
+    },
+    "STRAWBERRY" : {
+        "display_name": "STRAWBERRY",
+        "translation": [
+            
+        ]
+    },
+    // DAIRY
+    "UNSALTED_BUTTER": {
+        "display_name": "unsalted butter",
+        "translations": [
+
+        ]
+    },
+    "SALTED_BUTTER": {
+        "display_name": "salted butter",
+        "translations": [
+
+        ]
+    },
+    "CREME_FRAICHE": {
+        "display_name": "crème fraîche",
+        "translations": [
+            "creme fraiche"
+        ] 
+    },
+    "EGG": {
+        "display_name": "egg",
+        "translations": [
+
+        ]
+    },
+    "HALF_AND_HALF": {
+        "display_name": "half and half",
+        "translations": [
+
+        ]
+    },
+    "HEAVY WHIPPING CREAM": {
+        "display_name": "heavy whipping cream",
+        "translations": [
+            "heavy cream"
+        ]
+    },
+    "2%_MILK": {
+        "display_name": "2% milk",
+        "translations": [
+            "two percent milk",
+            "two-percent milk",
+            "2 percent milk"
+        ]
+    },
+    "1%_MILK": {
+        "display_name": "1% milk",
+        "translations": [
+            "one percent milk",
+            "one-percent milk",
+            "1 percent milk"
+        ]
+    },
+    "WHOLE_MILK": {
+        "display_name": "whole milk",
+        "translations": [
+        ]
+    },
+    "SKIM_MILK": {
+        "display_name": "skim milk",
+        "translations": [
+        ]
+    },
+    "ALMOND_MILK": {
+        "display_name": "almond milk",
+        "translations": [
+        ]
+    },
+    "SOY_MILK": {
+        "display_name": "soy milk",
+        "translations": [
+        ]
+    },
+    "OAT_MILK": {
+        "display_name": "oat milk",
+        "translations": [
+        ]
+    },
+    "SHREDDED_MOZZARELLA": {
+        "display_name": "shredded mozzarella cheese",
+        "translations": [
+            "shredded mozzarella"
+        ]
+    },
+    "SLICED_MOZZARELLA": {
+        "display_name": "sliced mozzarella cheese",
+        "translations": [
+            "sliced mozzarella"
+        ]
+    },
+    "PARMESAN": {
+        "display_name": "parmesan cheese",
+        "translations": [
+            "parmesan"
+        ]
+    },
+    "GRATED_PARMESAN": {
+        "display_name": "grated parmesan cheese",
+        "translations": [
+            "grated parmesan"
+        ]
+    },
+    "MANCHEGO": {
+        "display_name": "manchego cheese",
+        "translations": [
+            "manchego"
+        ]
+    },
+    "CHEDDAR": {
+        "display_name": "cheddar cheese",
+        "translations": [
+            "cheddar"
+        ]
+    },
+    "SHREDDED_CHEDDAR": {
+        "display_name": "shredded cheddar cheese",
+        "translations": [
+            "shredded cheddar"
+        ]
+    },
+    "EXTRA_SHARP_CHEDDAR": {
+        "display_name": "extra sharp cheddar cheese",
+        "translations": [
+            "extra sharp cheddar",
+            "extra-sharp cheddar",
+            "extra-sharp cheddar cheese",
+        ]
+    },
+
+    
+
+
+
+
+}
